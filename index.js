f9dc97dc56023e664f5030c894bf01dbbcd8d7b5
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-01-04 23:39:11 +0100

    Base

---
+++ index.js
@@ -0,0 +1,33 @@
+const lsHiddenItems = [];
+const lsKeysPressed = [];
+
+const onHandleClick = (theEvent) => {
+  if (lsKeysPressed.includes(16)) {
+    theEvent.preventDefault()
+    const item = (theEvent.srcElement);
+    item.style.display = "none"
+    lsHiddenItems.push(item);
+  }
+}
+
+window.onkeydown = function (e) {
+  const countHiddenItems = lsHiddenItems.length;
+
+  var key = e.keyCode ? e.keyCode : e.which;
+  lsKeysPressed.push(key);
+
+  if (countHiddenItems <= 0)
+    return;
+  if (lsKeysPressed.includes(90) && lsKeysPressed.includes(17)) {
+    lsHiddenItems[countHiddenItems - 1].style.display = "block";
+    lsHiddenItems.splice(countHiddenItems - 1, 1)
+  }
+}
+
+window.onkeyup = function (e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+  while (lsKeysPressed.includes(key))
+    lsKeysPressed.splice(lsKeysPressed.indexOf(key), 1);
+}
+
+document.addEventListener('click', onHandleClick)
